b68b33ec2d046465932cee9dce45e44573dfdd04
Vriyas Hartama <vriyashartama@gmail.com>
2020-11-12 06:06:05 +0700

    UGD Vue CLI

--- src/router.js
+++ src/router.js
@@ -27,6 +27,11 @@ const router = new VueRouter({
           name: "Guided",
           component: importComponent("TodoList/List"),
         },
+        {
+          path: "/ugd",
+          name: "Unguided",
+          component: importComponent("TodoList/ListUGD"),
+        },
       ],
     },
   ],
